6427b25422f3e15278e0a9454989def7a0b6eedd
BoogleCloud <ben.langrill@gmail.com>
2018-04-29 12:36:00 -0700

    more complete config with jira/confluence/certbot

--- nginx-setup-rhel.sh
+++ nginx-setup-rhel.sh
@@ -9,6 +9,14 @@ fi
 
 cp -f /tmp/nginx/nginx.conf /etc/nginx/nginx.conf
 
+# SSL Setup
+mkdir -p /etc/nginx/ssl
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
+# Create a temporary ssl cert until the letsencrypt one is installed
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt \
+    -subj "/C=US/ST=Michigan/L=Test/O=OrgName/OU=IT Department/CN=example.com"
+
 systemctl enable nginx
 systemctl start nginx
 
@@ -21,3 +29,8 @@ systemctl enable firewalld
 firewall-cmd --add-service=http
 firewall-cmd --add-service=https
 firewall-cmd --runtime-to-permanent
+
+# For final SSL configuration you'll need to login and run certbot-nginx
+#certbot --nginx -d boogle.cloud -d www.boogle.cloud
+# Then restart nginx to use the new key
+#systemctl restart nginx
